287a803bfd6c9cdf75348a4091ad49f71a540db1
thecocoblue <216339054+thecocoblue@users.noreply.github.com>
2026-03-31 17:30:33 +0000

    perf: optimize string casing logic in config loader
    Replace character-by-character loops with compiled regular expressions and list comprehensions in config key converters. Include fast-path early returns for already-converted strings.

--- nanobot/channels/slack.py
+++ nanobot/channels/slack.py
@@ -2,12 +2,11 @@
 
 import asyncio
 import re
-from typing import Any
 
 from loguru import logger
-from slack_sdk.socket_mode.websockets import SocketModeClient
 from slack_sdk.socket_mode.request import SocketModeRequest
 from slack_sdk.socket_mode.response import SocketModeResponse
+from slack_sdk.socket_mode.websockets import SocketModeClient
 from slack_sdk.web.async_client import AsyncWebClient
 
 from nanobot.bus.events import OutboundMessage
